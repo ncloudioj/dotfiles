50208de69388f031dd1be0dd9b06b922f4657ee4
Nan Jiang <njiang028@gmail.com>
2024-03-13 14:15:56 -0400

    Fix nvim-cmp icons

--- nvim/lua/settings/plugin/nvimcmp.lua
+++ nvim/lua/settings/plugin/nvimcmp.lua
@@ -65,34 +65,37 @@ cmp.setup({
     { name = "path" },
   }),
   formatting = {
-    format = function(entry, vim_item)
-      -- load lspkind icons
-      vim_item.kind = string.format("%s %s", lspkind.symbol_map[vim_item.kind], vim_item.kind)
-
-      vim_item.menu = ({
+    format = lspkind.cmp_format({
+      mode = 'symbol_text', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+                     -- can also be a function to dynamically calculate max width such as 
+                     -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+      ellipsis_char = '...', -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+      show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+      menu = ({
         nvim_lsp = "[LSP]",
         nvim_lua = "[Lua]",
         buffer = "[BUF]",
         luasnip = "[SNP]",
-      })[entry.source.name]
-
-      return vim_item
-    end,
+      }),
+    }),
   },
 })
 
 -- Use buffer source for `/`.
-cmp.setup.cmdline("/", {
+cmp.setup.cmdline({ "/", "?" }, {
+  mapping = cmp.mapping.preset.cmdline(),
   sources = {
     { name = "buffer" },
   },
 })
 
 -- Use cmdline & path source for ':'.
--- cmp.setup.cmdline(':', {
--- sources = cmp.config.sources({
--- { name = 'path' }
--- }, {
--- { name = 'cmdline' }
--- })
--- })
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
