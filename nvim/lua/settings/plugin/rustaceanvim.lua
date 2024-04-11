e9aec760ede77e9e0d5772883b384c993153808c
Nan Jiang <njiang028@gmail.com>
2024-04-10 23:05:51 -0400

    Add rustaceanvim configs

---
+++ nvim/lua/settings/plugin/rustaceanvim.lua
@@ -0,0 +1,23 @@
+local shared = require("settings.shared")
+
+vim.g.rustaceanvim = {
+  -- Plugin configuration
+  tools = {
+  },
+  -- LSP configuration
+  server = {
+    on_attach = function(client, bufnr)
+      -- you can also put keymaps in here
+      shared.on_attach(client, bufnr)
+      require("illuminate").on_attach(client)
+    end,
+    default_settings = {
+      -- rust-analyzer language server configuration
+      ['rust-analyzer'] = {
+      },
+    },
+  },
+  -- DAP configuration
+  dap = {
+  },
+}
