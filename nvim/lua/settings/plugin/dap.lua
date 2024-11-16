c6bd82e3fab06c52583e6b108f62081bf1cd285b
Nan Jiang <njiang028@gmail.com>
2024-11-15 23:49:40 -0500

    Update nvim

--- nvim/lua/settings/plugin/dap.lua
+++ nvim/lua/settings/plugin/dap.lua
@@ -2,16 +2,22 @@ require("dapui").setup()
 
 local dap, dapui = require("dap"), require("dapui")
 
-dap.listeners.after.event_initialized["dapui_config"] = function()
+dap.listeners.before.attach.dapui_config = function()
   dapui.open()
 end
-dap.listeners.before.event_terminated["dapui_config"] = function()
+dap.listeners.before.launch.dapui_config = function()
+  dapui.open()
+end
+dap.listeners.before.event_terminated.dapui_config = function()
   dapui.close()
 end
-dap.listeners.before.event_exited["dapui_config"] = function()
+dap.listeners.before.event_exited.dapui_config = function()
   dapui.close()
 end
 
 vim.keymap.set("n", "<Leader>dt", ":DapToggleBreakpoint<CR>")
 vim.keymap.set("n", "<Leader>dx", ":DapTerminate<CR>")
-vim.keymap.set("n", "<Leader>do", ":DapStepOver<CR>")
+vim.keymap.set("n", "<F9>", ":DapNew<CR>")
+vim.keymap.set("n", "<F10>", ":DapStepOver<CR>")
+vim.keymap.set("n", "<F11>", ":DapStepInto<CR>")
+vim.keymap.set("n", "<F12>", ":DapOut<CR>")
