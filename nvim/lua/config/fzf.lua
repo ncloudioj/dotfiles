231e5361577130085fe3ee6d33d4b540f8aadb5c
Nan Jiang <njiang028@gmail.com>
2025-04-25 07:47:07 -0400

    Update fzf-lua

--- nvim/lua/config/fzf.lua
+++ nvim/lua/config/fzf.lua
@@ -1,4 +1,6 @@
 require("fzf-lua").setup({
+  -- For better "resume"
+  { "hide" },
   previewers = {
     builtin = {
       -- Disable syntax highlighting for large files (100KB) 
@@ -13,8 +15,9 @@ vim.api.nvim_set_keymap("n", "bb", "<cmd>FzfLua buffers<CR>", opt_silent_noremap
 vim.api.nvim_set_keymap("n", "<C-p>", "<cmd>FzfLua files<CR>", opt_silent_noremap)
 
 vim.api.nvim_set_keymap("n", "<leader>ff", [[<cmd>FzfLua grep_cword<CR>]], opt_silent_noremap)
-vim.api.nvim_set_keymap("n", "<leader>fg", [[<cmd>FzfLua live_grep<CR>]], opt_silent_noremap)
+vim.api.nvim_set_keymap("n", "<leader>fg", [[<cmd>FzfLua live_grep_native<CR>]], opt_silent_noremap)
 vim.api.nvim_set_keymap("n", "<leader>fb", [[<cmd>FzfLua buffers<CR>]], opt_silent_noremap)
 vim.api.nvim_set_keymap("n", "<leader>fh", [[<cmd>FzfLua helptags<CR>]], opt_silent_noremap)
 vim.api.nvim_set_keymap("n", "<leader>ft", [[<cmd>FzfLua grep<CR>]], opt_silent_noremap)
 vim.api.nvim_set_keymap("n", "<leader>fr", [[<cmd>FzfLua lsp_references<CR>]], opt_silent_noremap)
+vim.api.nvim_set_keymap("n", "<leader>fa", [[<cmd>FzfLua resume<CR>]], opt_silent_noremap)
