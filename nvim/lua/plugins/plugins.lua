60d5f10973e35cb944b5a33dce0b52e87e257aa5
Nan Jiang <njiang028@gmail.com>
2026-08-04 09:33:22 -0400

    chore(nvim): update configs

--- nvim/lua/plugins/plugins.lua
+++ nvim/lua/plugins/plugins.lua
@@ -140,16 +140,6 @@ return {
   "nvim-lua/plenary.nvim",
   "nvim-lualine/lualine.nvim",
   "onsails/lspkind-nvim",
-  {
-    "OXY2DEV/markview.nvim",
-    lazy = true,      -- Recommended
-    ft = "markdown", -- If you decide to lazy-load anyway
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "nvim-tree/nvim-web-devicons"
-    }
-  },
-  "preservim/vim-markdown",
   "RRethy/vim-illuminate",
   "rafamadriz/friendly-snippets",
   {
@@ -177,7 +167,6 @@ return {
     lazy = false,
   },
   "ryanoasis/vim-devicons",
-  "suan/vim-instant-markdown",
   "terryma/vim-multiple-cursors",
   "tpope/vim-endwise",
   "tpope/vim-fugitive",
@@ -203,78 +192,5 @@ return {
     "nvim-telescope/telescope.nvim",
     version = "0.2.2",
     dependencies = { "nvim-lua/plenary.nvim" },
-  },
-  {
-    "yetone/avante.nvim",
-    build = vim.fn.has("win32") ~= 0
-        and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
-        or "make",
-    event = "VeryLazy",
-    version = false, -- Never set this value to "*"! Never!
-    opts = {
-      -- add any opts here
-      -- this file can contain specific instructions for your project
-      instructions_file = "avante.md",
-      -- for example
-      provider = "gemini",
-      providers = {
-        claude = {
-          endpoint = "https://api.anthropic.com",
-          model = "claude-sonnet-4",
-          api_key_name = "cmd:bw get notes anthropic-token",
-          timeout = 30000, -- Timeout in milliseconds
-          extra_request_body = {
-            temperature = 0.75,
-            max_tokens = 20480,
-          },
-        },
-        gemini = {
-          endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
-          model = "gemini-2.5-pro",
-          -- api_key_name = "cmd:bw get notes gemeni-token",
-          timeout = 30000, -- Timeout in milliseconds
-          temperature = 0,
-          max_tokens = 8192,
-        },
-      },
-    },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "echasnovski/mini.pick", -- for file_selector provider mini.pick
-      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
-      "ibhagwan/fzf-lua", -- for file_selector provider fzf
-      "stevearc/dressing.nvim", -- for input provider dressing
-      "folke/snacks.nvim", -- for input provider snacks
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
   },
 }
